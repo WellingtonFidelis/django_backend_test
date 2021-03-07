83e25d0d167f365e95d9605981660332f6916230
WellingtonFidelis <wellingtonf20@gmail.com>
2021-03-06 21:53:18 -0300

    method get, post, put and delete done, using apiView

--- api/views.py
+++ api/views.py
@@ -1,18 +1,52 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
+from rest_framework.views import APIView
 from .models import FormTest
 from .serializers import FormTestSerializer
 
 
 # Create your views here.
+# []create a status for all responses
+#
 
-class FormTestViewSet(viewsets.ModelViewSet):
+@api_view(['GET'])
+def formList(resquest):
     queryset = FormTest.objects.all()
-    serializer_class = FormTestSerializer
+    serializer = FormTestSerializer(queryset, many=True)
+    response = serializer.data
+    return Response(response)
 
-    @action(detail=True, methods=['GET'])
-    def createRegister(self, request, *args, **kwargs):
-        form = self.get_object()
-        return Response(form.createRegister)
+@api_view(['GET'])
+def formListDetail(request, pk):
+    queryset = FormTest.objects.get(id=pk)
+    serializer = FormTestSerializer(queryset, many=False)
+    response = serializer.data
+    return Response(response)
+
+@api_view(['POST'])
+def formCreate(request):
+    serializer = FormTestSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def formUpdate(request, pk):
+    queryset = FormTest.objects.get(id=pk)
+    serializer = FormTestSerializer(queryset, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def formDelete(request, pk):
+    queryset = FormTest.objects.get(id=pk)
+    queryset.delete()
+
+    return Response('Deleted')
